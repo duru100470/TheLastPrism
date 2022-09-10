3305ef1e58fd3e9268b73b1c630c1fdcffc9add9
duru100470 <zenpia@naver.com>
2022-09-11 08:31:04 +0900

    Basic terrain generation

---
+++ TheLastPrism/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [Header("World Setting")]
+    [SerializeField]
+    private int worldXSize;
+    [SerializeField]
+    private int worldYSize;
+    [SerializeField]
+    private int planeWidth;
+    [SerializeField]
+    private int seaWidth;
+    [SerializeField]
+    private int leftSeaDepth;
+    [SerializeField]
+    private int rightSeaDepth;
+    [SerializeField]
+    private int heightMultiplier;
+    [SerializeField]
+    private int heightAddition;
+    [SerializeField]
+    private float caveFreq;
+    [SerializeField]
+    private float terrainFreq;
+    [SerializeField]
+    private float seed;
+    [SerializeField]
+    private Texture2D noiseTexture;
+    [SerializeField]
+    private Sprite tile;
+
+    private void Start()
+    {
+        seed = Random.Range(-10000, 10000);
+        leftSeaDepth += (int)seed % 10;
+        rightSeaDepth += ((int)seed + 5) % 10;
+        GenerateNoiseTexture();
+        GenerateTerrain();
+    }
+
+    public void GenerateTerrain()
+    {
+        for (int x = 0; x < worldXSize; x++)
+        {
+            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+
+            if (x >= 0 && x < seaWidth)
+            {
+                height -= leftSeaDepth;
+            }
+            else if (x < (worldXSize / 2) - planeWidth)
+            {
+                height -= Mathf.SmoothStep(leftSeaDepth, 0, 
+                (float)(x - seaWidth) / (float)((worldXSize / 2 - planeWidth) - seaWidth));
+            }
+            else if (x >= (worldXSize / 2) + planeWidth && x < worldXSize - seaWidth)
+            {
+                height -= Mathf.SmoothStep(0, rightSeaDepth, 
+                (float)(x - ((worldXSize / 2) + planeWidth)) / (float)(worldXSize - seaWidth - ((worldXSize / 2) + planeWidth)));
+            }
+            else if (x >= worldXSize - seaWidth)
+            {
+                height -= rightSeaDepth;
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                if (noiseTexture.GetPixel(x, y).r > 0.2f)
+                {
+                    GameObject newTile = new GameObject(name = "tile");
+                    newTile.transform.parent = this.transform;
+                    newTile.AddComponent<SpriteRenderer>();
+                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
+                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                }
+            }
+        }
+    }
+
+    private void GenerateNoiseTexture()
+    {
+        noiseTexture = new Texture2D(worldXSize, worldYSize);
+
+        for (int x = 0; x < noiseTexture.width; x++)
+        {
+            for (int y = 0; y < noiseTexture.height; y++)
+            {
+                float v = Mathf.PerlinNoise((x + seed) * caveFreq, (y + seed) * caveFreq);
+                noiseTexture.SetPixel(x, y, new Color(v, v, v));
+            }
+        }
+
+        noiseTexture.Apply();
+    }
+}
