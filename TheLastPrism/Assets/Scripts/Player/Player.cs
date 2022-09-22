d3ea8ff4837ca1803d2e755daecd31c4f6171805
duru100470 <zenpia@naver.com>
2022-09-22 15:59:51 +0900

    Backup before import package

--- TheLastPrism/Assets/Scripts/Player/Player.cs
+++ TheLastPrism/Assets/Scripts/Player/Player.cs
@@ -102,7 +102,7 @@ public class Player : MonoBehaviour, IDamage
         playerController.stateMachine.SetState(new PlayerIdle(playerController));
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (!other.CompareTag("Item")) return;
 
